3040e7e9259fd2ea343ce43648d6575db95bbf06
Hector Lopez <hlopezcarballo@gmail.com>
2020-07-22 18:55:29 -0500

    Modified user-box animation

--- js/main.js
+++ js/main.js
@@ -1,14 +1,15 @@
-const share_action = document.querySelector(".share-icon");
-share_action.addEventListener("click", shareBox);
+const share_action = document.querySelectorAll(".share-icon");
+share_action[0].addEventListener("click", shareBox);
+share_action[1].addEventListener("click", shareBox);
 
 function shareBox() {
-  const user_box = document.querySelector(".user");
-  if (user_box.classList.contains("user--active")) {
-    user_box.classList.remove("user--active");
-    // user_box.setAttribute("style", "animation-name: move-in;");
+  const user_box = document.querySelector(".user--active");
+  if (user_box.classList.contains("move-in")) {
+    user_box.classList.remove("move-in");
+    user_box.classList.add("move-out");
   } else {
-    user_box.classList.add("user--active");
-    user_box.setAttribute("style", "");
+    user_box.classList.remove("move-out");
+    user_box.classList.add("move-in");
   }
   console.log("click");
 }
